700ff65756dd1e2cc3555e7a80b48831ed8da5b1
marvin <marvinmai@posteo.de>
2022-11-07 12:05:40 +0100

    Use System.out to log information about territory

--- src/main/java/de/unistuttgart/iste/sqa/pse/sheet03/presence/ExerciseHamsterGame.java
+++ src/main/java/de/unistuttgart/iste/sqa/pse/sheet03/presence/ExerciseHamsterGame.java
@@ -1,5 +1,6 @@
 package de.unistuttgart.iste.sqa.pse.sheet03.presence;
 
+import de.hamstersimulator.objectsfirst.datatypes.Size;
 import de.hamstersimulator.objectsfirst.external.model.Territory;
 import java.io.PrintStream;
 
@@ -19,11 +20,23 @@ public final class ExerciseHamsterGame extends InternalExerciseHamsterGame {
 	protected void hamsterRun() {
 		final Territory territory = game.getTerritory();
 		paule.write("Number of grains on field: " + territory.getTotalGrainCount());
-		paule.write("Number of hamsters on field: " + territory.getTotalHamsterCount());
-		paule.write("Size of territory: "
-				+ "Columns:" + territory.getTerritorySize().getColumnCount()
-				+ "Rows:" + territory.getTerritorySize().getRowCount());
+		output.append("Number of grains on field: ").append(String.valueOf(territory.getTotalGrainCount()))
+				.println();
 
+		paule.write("Number of hamsters on field: " + territory.getTotalHamsterCount());
+		output.append("Number of hamsters on field: ").append(String.valueOf(territory.getTotalHamsterCount()))
+				.println();
+
+		Size territorySize = territory.getTerritorySize();
+		paule.write("Size of territory: "
+				+ "Columns: " + territorySize.getColumnCount()
+				+ " Rows: " + territorySize.getRowCount()
+				+ "Size: " + territorySize.getRowCount() * territorySize.getColumnCount());
+		output.append("Size of territory: ")
+				.append("Columns: ").append(String.valueOf(territorySize.getColumnCount()))
+				.append(" Rows: ").append(String.valueOf(territorySize.getRowCount()))
+				.append("Size: ").append(String.valueOf(territorySize.getRowCount() * territorySize.getColumnCount()))
+				.println();
 	}
 
 	/**
